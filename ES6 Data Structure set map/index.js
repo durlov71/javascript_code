0c753858dae84aa28f0343ab5f3a29ad3795615c
Dark <istiak.csinfo@gmail.com>
2022-03-19 18:13:45 +0600

    Update index.js

--- ES6 Data Structure set map/index.js
+++ ES6 Data Structure set map/index.js
@@ -41,3 +41,28 @@ console.log(hasEl);
 
 map.delete('mango');
 console.log(map);
+
+
+// weak set : only supported object data type
+
+const ws = new WeakSet();
+const obj1 = {};
+const obj2 = {};
+ws.add(obj1);
+ws.add(obj2);
+console.log(ws);
+
+console.log(ws.has(obj1));
+console.log(ws.has(obj2));
+ws.delete(obj1);
+console.log(ws.has(obj1));
+
+//weak map 
+const wm = new WeakMap();
+wm.set(obj1, 123);
+
+console.log(wm.has(obj1));
+console.log(wm.get(obj1)); // print value of the obj1
+
+wm.delete(obj1);
+console.log(wm.has(obj1));
