df6aafd72912db89848085ea961a7678778dd3eb
Dark <istiak.csinfo@gmail.com>
2022-03-20 00:51:16 +0600

    Update func.js

--- ES6 Data Structure set map/func.js
+++ ES6 Data Structure set map/func.js
@@ -16,3 +16,16 @@ let name = '   john     ';
 console.log(name.trim().length);
 console.log(name.trimStart().length);
 console.log(name.trimEnd().length);
+
+
+//problem: Find the duplicate value of an array
+
+const numbers = [1, 2, 3, 3, 4, 5];
+let mapping = {};
+for (let x of numbers) {
+    if (mapping[x]) {
+        console.log(`${x} is a duplicate number`)
+    } else {
+        mapping[x] = true;
+    }
+}
